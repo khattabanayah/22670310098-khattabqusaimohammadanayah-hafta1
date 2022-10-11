5004981b36c086180590e451f2bc5337a7c39bbb
khattabanayah <114699820+khattabanayah@users.noreply.github.com>
2022-10-11 03:32:16 +0300

    Add files via upload

---
+++ Helloworld.java1.java
@@ -0,0 +1,10 @@
+import java.lang.runtime.ObjectMethods;
+
+public class Helloworld extends ObjectMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+	}
+
+}
